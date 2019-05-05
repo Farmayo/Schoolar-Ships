fe900c0e30c90cd15b214a817118281dd86797ae
Jair Jimenez <jairmunevar@gmai.com>
2019-05-05 12:13:07 -0500

    I don't know what this is

--- BecasSibate-ejb/src/java/Controller/Test.java
+++ BecasSibate-ejb/src/java/Controller/Test.java
@@ -22,5 +22,4 @@ public class Test {
     public static void main(String[] args) {
         new Test();
     }
-    
 }
--- BecasSibate-ejb/src/java/DTO/PhoneDTO.java
+++ BecasSibate-ejb/src/java/DTO/PhoneDTO.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DTO;
 
-/**
- *
- * @author Hermanos-Jimenez
- */
 public class PhoneDTO {
     private int phone_id;
     private String number;
@@ -52,6 +44,4 @@ public class PhoneDTO {
     public void setUser_id(String user_id) {
         this.user_id = user_id;
     }
-    
-    
 }
